28e3f6b08169ca7fdc3fbd693ef1d71a322140e5
Ivan Kuznetsov <me@jeiwan.ru>
2021-06-21 16:36:03 +0700

    Simulate increased ECR

---
+++ simulate_ecr.js
@@ -0,0 +1,29 @@
+const data = require("./data.json");
+const { arbProfit } = require("./helpers.js");
+
+const newData = Object.entries(data).reduce(function (m, p) {
+  const [k, v] = p;
+  const simECR = (parseFloat(v["ECR"]) * 1.1).toFixed(4);
+  const simArbProfit =
+    parseFloat(v["IRON price"]) > 1.0
+      ? null
+      : arbProfit(
+          parseFloat(v["IRON price"]),
+          parseFloat(v["TITAN price"]),
+          parseFloat(v["TITAN price, AMM"]),
+          simECR
+        );
+
+  v["Arb profit, simulated"] = simArbProfit;
+  v["ECR, simulated"] = simECR;
+
+  delete v["TCR"];
+  delete v["IRON total supply"];
+  delete v["TITAN total supply"];
+
+  m[k] = v;
+
+  return m;
+}, {});
+
+console.table(newData);
