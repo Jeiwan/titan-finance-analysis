15f901be06480fa06bd59853951594844e06a5bb
Ivan Kuznetsov <me@jeiwan.ru>
2021-06-22 18:41:18 +0700

    Add full historical metrics

---
+++ historical_cr.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+const Web3 = require("web3");
+const fetch = require("node-fetch");
+const treasuryABI = require("./treasury.json");
+const { formatDate } = require("./helpers.js");
+
+const utils = Web3.utils;
+const treasuryAddress = "0x376b9e0abbde0ca068defcd8919ca73369124825";
+const nodeURL = process.env.ARCHIVE_NODE_URL; // Get one from https://moralis.io/, use a 'Mainnet Archive' speedy Polygon node
+
+const timestampToBlockNum = async (timestamp) => {
+  return fetch(
+    `https://api.polygonscan.com/api?module=block&action=getblocknobytime&timestamp=${timestamp}&closest=before&apikey=YourApiKeyToken`
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.status === "1") {
+        return parseInt(res.result);
+      } else {
+        throw Error(res.message);
+      }
+    });
+};
+
+// ============================================================
+
+const web3 = new Web3(new Web3.providers.HttpProvider(nodeURL));
+const treasury = new web3.eth.Contract(treasuryABI, treasuryAddress);
+
+const hour = 3600;
+const startTime = Math.ceil(1622274115 / hour) * hour; // Treasury creation time
+const endTime = Math.ceil(1623891602 / hour) * hour; // June 17, 2 AM
+
+timestampToBlockNum(endTime);
+
+(async () => {
+  const data = JSON.parse(fs.readFileSync("data_cr.json"));
+
+  for (let i = 1623416400 /*startTime*/; i <= endTime; i += hour) {
+    console.log("Collecting data, timestamp ", i);
+
+    const blockNumber = await timestampToBlockNum(i);
+
+    const [block, dollarPrice, sharePrice, tcr, ecr] = await Promise.all([
+      web3.eth.getBlock(blockNumber),
+      treasury.methods.dollarPrice().call({}, blockNumber),
+      treasury.methods.sharePrice().call({}, blockNumber),
+      treasury.methods.target_collateral_ratio().call({}, blockNumber),
+      treasury.methods.effective_collateral_ratio().call({}, blockNumber),
+    ]);
+
+    data[blockNumber] = {
+      Date: formatDate(new Date(block.timestamp * 1000)),
+      "IRON price": utils.fromWei(dollarPrice, "mwei"),
+      "TITAN price": utils.fromWei(sharePrice, "mwei"),
+      TCR: parseFloat(utils.fromWei(tcr, "mwei")).toFixed(6),
+      ECR: parseFloat(utils.fromWei(ecr, "mwei")).toFixed(6),
+    };
+
+    fs.writeFileSync("data_cr.json", JSON.stringify(data));
+  }
+
+  console.table(data);
+})();
---
+++ historical_cr.py
@@ -0,0 +1,42 @@
+import json
+import matplotlib.pyplot as plt
+
+with open("data_cr.json", "r") as file:
+    crdata = json.loads(file.read())
+
+x = list(crdata)
+x.sort()
+
+y1 = [float(crdata[k]["TCR"]) for k in x]
+y2 = [float(crdata[k]["ECR"]) for k in x]
+y3 = [float(crdata[k]["TITAN price"]) for k in x]
+y4 = [1.0 if float(crdata[k]["IRON price"]) > 1.0 else None for k in x]
+y5 = [1.0 if float(crdata[k]["IRON price"]) <= 1.0 else None for k in x]
+
+
+fig, ax = plt.subplots()
+
+line1 = ax.plot(x, y1, label="TCR")
+line2 = ax.plot(x, y2, label="ECR")
+
+ax.set_xlabel("Time")
+ax.set_ylabel("ECR/TCR")
+
+ax2 = ax.twinx()
+line3 = ax2.plot(x, y3, label="TITAN price", color="red")
+
+ax2.set_ylabel("TITAN price, $")
+
+ax3 = ax.twinx()
+scat1 = ax3.scatter(x, y4, label="IRON > $1", s=1, color="green")
+scat2 = ax3.scatter(x, y5, label="IRON <= $1", s=1, color="red")
+
+ax3.set_ylim(ymin=0, ymax=60)
+ax3.set_yticks([])
+
+ax.set_xticks([x[0], x[-1]])
+ax.set_xticklabels(["May 29", "June 17"])
+
+fig.legend(loc="upper left", bbox_to_anchor=(0, 1), bbox_transform=ax.transAxes)
+
+plt.show()
